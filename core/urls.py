d5aa71a998ffca8dfa1ba054d9564c6b81787c14
septa-ma <maryamasadi.septa@gmail.com>
2023-11-03 19:11:29 +0330

    120802

--- core/urls.py
+++ core/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app.urls')),
+    path('app/', include('app.urls')),
+    path('search/', include('search.urls')), 
 ]
